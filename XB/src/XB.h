961f3881a47f899e63ec114fe1cabad28c077ba9
Vasil Tinkov <vasil.tinkov1@gmail.com>
2026-06-04 09:06:39 +0200

    Added basic Logging

--- XB/src/XB.h
+++ XB/src/XB.h
@@ -3,6 +3,7 @@
 // For use by XB applications only.
 
 #include "XB/Application.h"
+#include "XB/Log.h"
 
 // ---Entry Point-----------------
 #include "XB/EntryPoint.h"
--- XB/src/XB/EntryPoint.h
+++ XB/src/XB/EntryPoint.h
@@ -6,6 +6,11 @@ extern XB::Application* XB::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	XB::Log::Init();
+	XB_CORE_INFO("Initialized Log!");
+	int a = 5;
+	XB_INFO("Hello! we gucci. Var={0}", a);
+
 	auto app = XB::CreateApplication();
 	app->Run();
 	delete app;
